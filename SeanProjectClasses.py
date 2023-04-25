ad9af7451128c3794fe1538042dae8fe8b321567
Sean Wagner <sean_wagner@outlook.com>
2023-04-24 18:52:55 -0600

    Updating repository

---
+++ SeanProjectClasses.py
@@ -0,0 +1,30 @@
+'''
+Group Members: Keith Payuran, Ghunder Tayupon, Sean Wagner
+'''
+
+class Doctor:
+    def Constructor(self):
+
+    def Getters(self):
+
+    def Setters(self):
+
+    def __str__(self):
+
+
+class DoctorManager:
+    def Constructor(self):
+
+    def format_dr_info(self):
+
+    def enter_dr_info(self):
+
+    def read_doctors_file(self):
+
+    def search_doctor_by_id(self):
+
+    def search_doctor_by_name(self):
+
+    def display_doctor_info(self):
+
+    def
