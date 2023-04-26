a6f6e7dea97bcaa546b33e1390ac38624f5117f7
Sean Wagner <sean_wagner@outlook.com>
2023-04-26 10:35:44 -0600

    Final edits before video demonstration

--- MasterProjectClasses.py
+++ MasterProjectClasses.py
@@ -1,13 +1,14 @@
 """
 Project Classes
+Due April 26th, 2023 @11:59PM
 Group Members: Keith Payuran, Ghunder Tayupon, Sean Wagner
 
 Note: On original "doctors.txt" file, we changed spelling of specililty (in file) to speciality (in program) - Attached
 """
 
 
-class Doctor:
-    def __init__(self, doctor_id="", name="", specialization="", working_time="", qualification="", room_number=""):
+class Doctor:  # creates doctor object, class #1
+    def __init__(self, doctor_id="", name="", specialization="", working_time="", qualification="", room_number=""):  # doctor properties
         self.doctor_id = doctor_id
         self.name = name
         self.specialization = specialization
@@ -55,25 +56,26 @@ class Doctor:
         return f"{self.doctor_id}_{self.name}_{self.specialization}_{self.working_time}_{self.qualification}_{self.room_number}"
 
 
-class DoctorManager:
+class DoctorManager:  # used by display_doctor_menu for managing doctor, class #2
 
     def __init__(self):
         self.doctors = []
         self.read_doctors_file()
 
-    def format_dr_info(self, doctor):
+    def format_dr_info(self, doctor):  # formats input into the usable format for the text file
         return f"{doctor.doctor_id}_{doctor.name}_{doctor.specialization}_{doctor.working_time}_{doctor.qualification}_{doctor.room_number}\n"
 
-    def enter_dr_info(self):
+    def enter_dr_info(self):  # enter in data for a new doctor
         doc_id = input("Enter the doctor\'s ID:\t")
         doc_name = input("Enter the doctor\'s name:\t")
         doc_spec = input("Enter the doctor\'s speciality:\t")
         doc_timing = input("Enter the doctor\'s timing (e.g., 7am-10pm):\t")
         doc_qual = input("Enter the doctor\'s qualification:\t")
         doc_rm_num = input("Enter the doctor\'s room number:\t")
+        print(" ")
         return Doctor(doc_id, doc_name, doc_spec, doc_timing, doc_qual, doc_rm_num)
 
-    def read_doctors_file(self):
+    def read_doctors_file(self):  # used with Doctor to create objects from the file
         with open("doctors.txt", "r") as f:
             next(f)
             for line in f:
@@ -81,25 +83,31 @@ class DoctorManager:
                 doctor = Doctor(doc_id, doc_name, doc_spec, doc_timing, doc_qual, doc_rm_num)
                 self.doctors.append(doctor)
 
-    def search_doctor_by_id(self):
+    def search_doctor_by_id(self):  # searches database for doctor by their ID
         doc_id = input("\nEnter the doctor's ID:\t")
+        print(" ")
+        print("Doctor ID\tName\tSpecialization\tWorking Time\tQualification\tRoom Number")
         for doctor in self.doctors:
             if doctor.get_doctor_id() == doc_id:
                 self.display_doctor_info(doctor)
+                print(" ")
                 return
         else:
             return print("Can't find the doctor with the same ID on the system.")
 
-    def search_doctor_by_name(self):
+    def search_doctor_by_name(self):  # searches database for doctor by their name
         doc_name = input("\nEnter the doctor's name:\t")
+        print(" ")
+        print("Doctor ID\tName\tSpecialization\tWorking Time\tQualification\tRoom Number")
         for doctor in self.doctors:
             if doctor.get_name() == doc_name:
                 self.display_doctor_info(doctor)
+                print(" ")
                 return
         else:
             print("Can't find the doctor with the same name on the system.")
 
-    def display_doctor_info(self, doctor):
+    def display_doctor_info(self, doctor):  # displays individual doctor's profile
         print(f"\n{doctor.doctor_id}\t", end="")
         print(f"{doctor.name}\t", end="")
         print(f"{doctor.specialization}\t", end="")
@@ -107,7 +115,7 @@ class DoctorManager:
         print(f"{doctor.qualification}\t", end="")
         print(f"{doctor.room_number}\t", end="")
 
-    def edit_doctor_info(self):
+    def edit_doctor_info(self):  # edits doctors profile, then adds back to file
         doc_id = input("Please enter the id of the doctor that you want to edit their information:\t")
         for doctor in self.doctors:
             if doctor.doctor_id == doc_id:
@@ -121,18 +129,18 @@ class DoctorManager:
                 return
         print("Cannot find the Doctor with the same id on the system")
 
-    def display_doctors_list(self):
+    def display_doctors_list(self):  # shows the list of doctors in the file
         print("Doctor ID\tName\tSpecialization\tWorking Time\tQualification\tRoom Number")
         for doctor in self.doctors:
             self.display_doctor_info(doctor)
             print(" ")
 
-    def write_list_of_doctors_to_file(self):
+    def write_list_of_doctors_to_file(self):  # writes the list of doctors to the file
         with open("doctors.txt", "w") as f:
             for doctor in self.doctors:
                 f.write(self.format_dr_info(doctor))
 
-    def add_dr_to_file(self):
+    def add_dr_to_file(self):  # adds doctor to the file
         doctor = self.enter_dr_info()
         self.doctors.append(doctor)
         with open("doctors.txt", "a") as f:
@@ -140,8 +148,8 @@ class DoctorManager:
         return print(f"Doctor whose ID is {doctor.doctor_id} has been added.")
 
 
-class Patient:
-    def __init__(self, pid="", name="", disease="", gender="", age=""):
+class Patient:  # creates patient object, class #3
+    def __init__(self, pid="", name="", disease="", gender="", age=""):  # properties of patient objects
         self.pid = pid
         self.name = name
         self.disease = disease
@@ -182,15 +190,15 @@ class Patient:
         return f"{self.pid}_{self.name}_{self.disease}_{self.gender}_{self.age}"
 
 
-class PatientManager:
+class PatientManager:  # used by display_patient_menu for managing patient, class #4
     def __init__(self):
         self.patients = []
         self.read_patients_file()
 
-    def format_patient_info_for_file(self, patient):
+    def format_patient_info_for_file(self, patient):  # formats input into usable code for the text file
         return f"{patient.pid}_{patient.name}_{patient.disease}_{patient.gender}_{patient.age}\n"
 
-    def enter_patient_info(self):
+    def enter_patient_info(self):  # used with Patient, gathers information for the object
         pid = input("\nEnter patient ID: ")
         name = input("Enter patient name: ")
         disease = input("Enter patient disease: ")
@@ -199,7 +207,7 @@ class PatientManager:
         print(" ")
         return Patient(pid, name, disease, gender, age)
 
-    def read_patients_file(self):
+    def read_patients_file(self):  # opens patients.txt, skips first line and reads the rest of the data
         with open("patients.txt", "r") as f:
             next(f)
             for line in f:
@@ -207,8 +215,10 @@ class PatientManager:
                 patient = Patient(pid, name, disease, gender, age)
                 self.patients.append(patient)
 
-    def search_patient_by_id(self):
+    def search_patient_by_id(self):  # searches database for patient's ID
         pid = input("\nEnter patient ID: ")
+        print(" ")
+        print("Patient ID\tName\tDisease\tGender\tAge")
         for patient in self.patients:
             if patient.pid == pid:
                 self.display_patient_info(patient)
@@ -216,14 +226,14 @@ class PatientManager:
                 return
         print(f"Can't find the Patient with the same id on the system")
 
-    def display_patient_info(self, patient):  # error here
+    def display_patient_info(self, patient):  # displays individual patient's information
         print(f"\n{patient.pid}\t", end="")
         print(f"{patient.name}\t", end="")
         print(f"{patient.disease}\t", end="")
         print(f"{patient.gender}\t", end="")
         print(f"{patient.age}\t", end="")
 
-    def edit_patient_info_by_id(self):
+    def edit_patient_info_by_id(self):  # search for patient's ID, then edit their profile
         pid = input("\nPlease enter the id of the Patient that you want to edit their information: ")
         for patient in self.patients:
             if patient.pid == pid:
@@ -237,18 +247,18 @@ class PatientManager:
                 return
         print(f"Can't find the patient with the same id in the system")
 
-    def display_patients_list(self):  # for this function we need to skip the first entry
+    def display_patients_list(self):  # displays list of patients
         print("Patient ID\tName\tDisease\tGender\tAge")
         for patient in self.patients:
             self.display_patient_info(patient)
             print(" ")
 
-    def write_list_of_patients_to_file(self):
+    def write_list_of_patients_to_file(self):  # writes the list of patients to the file
         with open("patients.txt", "w") as f:
             for patient in self.patients:
                 f.write(self.format_patient_info_for_file(patient))
 
-    def add_patient_to_file(self):
+    def add_patient_to_file(self):  # adds patient to the file
         patient = self.enter_patient_info()
         self.patients.append(patient)
         with open("patients.txt", "a") as f:
@@ -256,26 +266,26 @@ class PatientManager:
         print(f"Patient whose ID is {patient.pid} has been added.\n")
 
 
-class Management:
+class Management:  # inital menu, then able to select either doctors/patients, class #5
     def __init__(self):
         self.doctor_manager = DoctorManager()
         self.patient_manager = PatientManager()
 
-    def display_menu(self):
+    def display_menu(self):  # Main Menu for program
         while True:
-            choice1 = input("Welcome to Alberta Hospital (AH) Management System\nSelect from the following options, or "
+            choice1 = input("\nWelcome to Alberta Hospital (AH) Management System\nSelect from the following options, or "
                             "select 3 to stop:\n1 -  Doctors\n2 -  Patients\n3 -  Exit Program\n")
-            if choice1 == "1":
+            if choice1 == "1":  # opens doctor menu
                 self.display_doctor_menu()
-            elif choice1 == "2":
+            elif choice1 == "2":  # opens patient menu
                 self.display_patient_menu()
-            elif choice1 == "3":
+            elif choice1 == "3":  # ends program
                 print("\nThanks for using the program! Bye")
                 break
             else:
                 print("Invalid choice. Please try again.")
 
-    def display_doctor_menu(self):
+    def display_doctor_menu(self):  # displays doctor menu
         while True:
             choice = input("\nDoctor Menu:\n1 - Display Doctors list\n2 - Search for doctor by ID\n3 - "
                            "Search for doctor by name\n4 - Add doctor\n5 - Edit doctor info\n6 - Back to Main Menu\n")
@@ -291,12 +301,12 @@ class Management:
             elif choice == "5":
                 self.doctor_manager.edit_doctor_info()
                 self.doctor_manager.write_list_of_doctors_to_file()
-            elif choice == "6":
+            elif choice == "6":  # back to main menu
                 break
             else:
                 print("Invalid choice. Please try again.")
 
-    def display_patient_menu(self):
+    def display_patient_menu(self):  # displays patient menu
         while True:
             choice = input("\nPatient Menu:\n1 - Display patients list\n2 - Search for patient by ID\n3 - Add patient\n"
                            "4 - Edit patient info\n5 - Back to the Main Menu\n")
@@ -310,11 +320,11 @@ class Management:
             elif choice == "4":
                 self.patient_manager.edit_patient_info_by_id()
                 self.patient_manager.write_list_of_patients_to_file()
-            elif choice == "5":
+            elif choice == "5":  # back to main menu
                 break
             else:
                 print("Invalid choice. Please try again.")
 
 
-run = Management()
-run.display_menu()
+run = Management()  # creates object from Management
+run.display_menu()  # runs display_menu
